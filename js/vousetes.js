b61b22d1735f0e00798b4734ab41ecff34f69f05
thibaulthans <thibault.hans@hei.fr>
2017-02-15 12:15:07 +0100

    test

---
+++ js/vousetes.js
@@ -0,0 +1,18 @@
+
+
+window.onload=function(){
+
+
+function hidden_effect_click() {
+
+if(document.querySelector(".modal").onclick){
+     document.getElementById("jscarre").className='shadow_y';;
+}
+
+if(document.querySelector(".btn-circle").onclick){
+        document.getElementById("jscarre").className='shadow_n';
+}
+
+}
+
+}
