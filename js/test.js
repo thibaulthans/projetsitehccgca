1bc39bc05c43a6563b5549d0babc5f3152f94995
thibaulthans <thibault.hans@hei.fr>
2017-02-17 16:31:47 +0100

    logo bar menu

--- js/test.js
+++ js/test.js
@@ -2,25 +2,19 @@
 
 
 
-
-function testScroll(ev){
-  var elmt = document.querySelector(".navbar-default");
-    if(window.pageYOffset<100){
-      elmt.style.paddingTop="5px";
-      elmt.style.paddingBottom="5px";
-    }
-    if(window.pageYOffset>100){
-      elmt.style.paddingTop="0px";
-      elmt.style.paddingBottom="0px";
-    }
-    if(window.pageYOffset>200){
-      elmt.style.display="none";
-    }
-    if(window.pageYOffset<200){
-      elmt.style.display="block";
-    }
-    document.querySelector(".navbar-default").style.WebkitTransition = "all 0.9s"; // Code for Safari 3.1 to 6.0
-    document.getElementById(".navbar-default").style.transition = "all 0.9s";
-
-}
-window.onscroll=testScroll
+$(function() {
+  $('.btn-6')
+    .on('mouseenter', function(e) {
+			var parentOffset = $(this).offset(),
+      		relX = e.pageX - parentOffset.left,
+      		relY = e.pageY - parentOffset.top;
+			$(this).find('span').css({top:relY, left:relX})
+    })
+    .on('mouseout', function(e) {
+			var parentOffset = $(this).offset(),
+      		relX = e.pageX - parentOffset.left,
+      		relY = e.pageY - parentOffset.top;
+    	$(this).find('span').css({top:relY, left:relX})
+    });
+  $('[href=a]').click(function(){return false});
+});
