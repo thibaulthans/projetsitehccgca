1bcca01bd625ef771f524be048d22f9eff3d8d85
thibaulthans <thibault.hans@hei.fr>
2017-02-19 22:05:45 +0100

    avancée globale

--- js/vousetes.js
+++ js/vousetes.js
@@ -1,18 +1,82 @@
 
 
-window.onload=function(){
 
+$('.c1').mouseover(function() {
+      $('.g1').addClass('scale_gly');
+      $('.g1').removeClass('scale_gly_after');
+})
 
-function hidden_effect_click() {
+$('.c1').mouseout(function() {
+    $('.g1').removeClass('scale_gly');
+    $('.g1').addClass('scale_gly_after');
+})
 
-if(document.querySelector(".modal").onclick){
-     document.getElementById("jscarre").className='shadow_y';;
-}
+$('.c2').mouseover(function() {
+      $('.g2').addClass('scale_gly');
+      $('.g2').removeClass('scale_gly_after');
+})
 
-if(document.querySelector(".btn-circle").onclick){
-        document.getElementById("jscarre").className='shadow_n';
-}
+$('.c2').mouseout(function() {
+    $('.g2').removeClass('scale_gly');
+    $('.g2').addClass('scale_gly_after');
+})
 
-}
+$('.c3').mouseover(function() {
+      $('.g3').addClass('scale_gly');
+      $('.g3').removeClass('scale_gly_after');
+})
 
-}
+$('.c3').mouseout(function() {
+    $('.g3').removeClass('scale_gly');
+    $('.g3').addClass('scale_gly_after');
+})
+
+$('.c4').mouseover(function() {
+      $('.g4').addClass('scale_gly');
+      $('.g4').removeClass('scale_gly_after');
+})
+
+$('.c4').mouseout(function() {
+    $('.g4').removeClass('scale_gly');
+    $('.g4').addClass('scale_gly_after');
+})
+
+$('.c5').mouseover(function() {
+      $('.g5').addClass('scale_gly');
+      $('.g5').removeClass('scale_gly_after');
+})
+
+$('.c5').mouseout(function() {
+    $('.g5').removeClass('scale_gly');
+    $('.g5').addClass('scale_gly_after');
+})
+
+$('.c6').mouseover(function() {
+      $('.g6').addClass('scale_gly');
+      $('.g6').removeClass('scale_gly_after');
+})
+
+$('.c6').mouseout(function() {
+    $('.g6').removeClass('scale_gly');
+    $('.g6').addClass('scale_gly_after');
+})
+
+$('.c7').mouseover(function() {
+      $('.g7').addClass('scale_gly');
+      $('.g7').removeClass('scale_gly_after');
+})
+
+$('.c7').mouseout(function() {
+    $('.g7').removeClass('scale_gly');
+    $('.g7').addClass('scale_gly_after');
+})
+
+$('.c8').mouseover(function() {
+      $('.g8').addClass('scale_gly');
+      $('.g8').removeClass('scale_gly_after');
+})
+
+$('.c8').mouseout(function() {
+    $('.g8').removeClass('scale_gly');
+    $('.g8').addClass('scale_gly_after');
+})
