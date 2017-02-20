45cff56a42c35469d1a923cbfa5447bdd27cfa2d
thibaulthans <thibault.hans@hei.fr>
2017-02-20 15:33:08 +0100

    logo move

--- js/home.js
+++ js/home.js
@@ -4,12 +4,20 @@ $('#btn_hcc').mouseover(function() {
       $('#letter1').addClass('letter_scale_plus');
       $('#letter2').addClass('letter_scale_plus');
       $('#letter3').addClass('letter_scale_plus');
+      $(".div_g").css({ "height": "30.5px"});
+      $(".div_g").css({ "padding-left": "63.5px"});
+      $(".div_a").css({"padding-left": "64px"});
+      $(".div_a").css({"margin-top": "-10.5px"});
 })
 
 $('#btn_hcc').mouseout(function() {
   $('#letter1').removeClass('letter_scale_plus');
   $('#letter2').removeClass('letter_scale_plus');
   $('#letter3').removeClass('letter_scale_plus');
+  $(".div_g").css({ "height": "34px"});
+  $(".div_g").css({ "padding-left": "62.5px"});
+  $(".div_a").css({"padding-left": "62.5px"});
+  $(".div_a").css({"margin-top": "-12.5px"});
 })
 
 $('#btn_gca').mouseover(function() {
